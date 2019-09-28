7785f0cce67ef7a45ace5e1a32d10cf284f916bc
Ruben Nati <ruben.nati@me.com>
2019-09-28 15:23:22 +0200

    Add basic collection

--- src/basic-collection.php
+++ src/basic-collection.php
@@ -1,1 +1,16 @@
 <?php
+
+// Cross-Site Scripting (XSS)
+$name = $_GET['name'];
+echo('Hello ' . $name);
+
+// SQL Injection
+$id = $_POST['id'];
+mysql_query("SELECT user FROM users WHERE id = " . $id);
+
+// Command Injection
+$cmd = $_COOKIE['cmd'];
+exec("cat /var/log/apache2/access.log | grep " . $cmd);
+
+// Deprecated Function
+$words = split(":", "split:this");
