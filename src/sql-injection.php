8218a33928ba35ee18af54b5298d2d61fb9912b7
kumar raju <kumar10raju2@gmail.com>
2025-03-20 11:44:58 +0530

    Update sql-injection.php

--- src/sql-injection.php
+++ src/sql-injection.php
@@ -8,7 +8,7 @@ $file_db = new PDO('sqlite:../database/database.sqlite');
 
 if (NULL == $_GET['id']) $_GET['id'] = 1;
 
-$sql = 'SELECT * FROM employees WHERE employeeId = ' . $_GET['id'];
+$sql = 'SELECT * FROM employees WHERE employeeId = ' . $_GET['id:'];
 
 foreach ($file_db->query($sql) as $row) {
     $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
